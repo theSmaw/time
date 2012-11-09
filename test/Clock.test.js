c4103b5de77fcdf4025b69f3f7a64d3e66b3188f
Ben Smawfield <bensmawfield@notonthehighstreet.com>
2012-11-09 17:36:09 +0000

    failing test - telling the time

--- test/Clock.test.js
+++ test/Clock.test.js
@@ -1,6 +1,22 @@
 describe('Clock', function () {
+    var clock;
+
+    function before () {
+        clock = new Clock();
+    }
 
     it('should be a function', function () {
        expect(typeof(Clock)).toBe('function');
+    });
+
+    describe('instance of clock', function () {
+        beforeEach(before);
+
+        describe('telling the time', function () {
+
+            it('should have a tell the time method', function () {
+                expect(clock.tellTheTime).toBeDefined();
+            });
+        });
     });
 });
