2c8815b8e7a371208e5800dd7c4d0f59e47ede33
Ben Smawfield <bensmawfield@googlemail.com>
2012-11-11 17:18:38 +0000

    failing test - converting minutes

--- test/Minutes.test.js
+++ test/Minutes.test.js
@@ -8,4 +8,12 @@ describe('Minutes', function () {
     it('should be a function', function () {
        expect(typeof(Minutes)).toBe('function');
     });
+    
+    describe('instance of Minutes', function () {
+        beforeEach(before);
+
+        it('should have a convert method', function () {
+            expect(minutes.convert).toBeDefined();
+        });
+    });
 });
