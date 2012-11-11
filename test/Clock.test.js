40e59b3fe52a98ed88c0fe7d913d8de491be11eb
Ben Smawfield <bensmawfield@googlemail.com>
2012-11-11 11:11:53 +0000

    failing test - returning an hour if there are no minutes and it is am

--- test/Clock.test.js
+++ test/Clock.test.js
@@ -19,10 +19,49 @@ describe('Clock', function () {
             });
 
             it('should return a string', function () {
-                var str;
+                var time;
 
-                str = clock.tellTheTime();
-                expect(typeof(str)).toBe('string');
+                time = clock.tellTheTime();
+                expect(typeof(time)).toBe('string');
+            });
+            
+            it('should return the hour only if there are no minutes and it is AM', function () {
+                var rawTime = new Date(),
+                    time;
+                    
+                rawTime.setHours(1, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(rawTime).toBe('one o\'clock');
+                rawTime.setHours(2, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('two o\'clock');
+                rawTime.setHours(3, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('three o\'clock');
+                rawTime.setHours(4, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('four o\'clock');
+                rawTime.setHours(5, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('five o\'clock');
+                rawTime.setHours(6, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('six o\'clock');
+                rawTime.setHours(7, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('seven o\'clock');
+                rawTime.setHours(8, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('eight o\'clock');
+                rawTime.setHours(9, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('nine o\'clock');
+                rawTime.setHours(10, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('ten o\'clock');
+                rawTime.setHours(11, 0, 0, 0);                
+                time = clock.tellTheTime(rawTime);
+                expect(hour).toBe('eleven o\'clock');
             });
         });
     });
