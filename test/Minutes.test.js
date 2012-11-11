809693ee749ee90130cdf63967f617a8ac198acf
Ben Smawfield <bensmawfield@googlemail.com>
2012-11-11 18:22:32 +0000

    failing test - just before prefix

--- test/Minutes.test.js
+++ test/Minutes.test.js
@@ -29,7 +29,6 @@ describe('Minutes', function () {
             expect(convertedMinutes.prefix).toBe('just after');
             convertedMinutes = minutes.convert(2);
             expect(convertedMinutes.prefix).toBe('just after');
-            convertedMinutes = minutes.convert(2);
         });
         
         it('should return a prefix rounded to the nearest 5 minutes for all times up to 3 minues before the next hour', function () {
@@ -40,46 +39,54 @@ describe('Minutes', function () {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('five past');
             }
-            for (i = 8; i < 8; i += 1) {
+            for (i = 8; i < 13; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('ten past');
             }
-            for (i = 13; i < 8; i += 1) {
+            for (i = 13; i < 18; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('a quarter past');
             }
-            for (i = 18; i < 8; i += 1) {
+            for (i = 18; i < 23; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('twenty past');
             }
-            for (i = 23; i < 8; i += 1) {
+            for (i = 23; i < 28; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('twenty-five minutes past');
             }
-            for (i = 28; i < 8; i += 1) {
+            for (i = 28; i < 33; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('half past');
             }
-            for (i = 33; i < 8; i += 1) {
+            for (i = 33; i < 38; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('twenty-five minutes to');
             }
-            for (i = 38; i < 8; i += 1) {
+            for (i = 38; i < 43; i += 1) {
                 convertedMinutes = minutes.convert(i);
-                expect(convertedMinutes.prefix).toBe('twenty two');
+                expect(convertedMinutes.prefix).toBe('twenty to');
             }
-            for (i = 43; i < 8; i += 1) {
+            for (i = 43; i < 48; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('a quarter to');
             }
-            for (i = 48; i < 8; i += 1) {
+            for (i = 48; i < 53; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('ten to');
             }
-            for (i = 53; i < 8; i += 1) {
+            for (i = 53; i < 58; i += 1) {
                 convertedMinutes = minutes.convert(i);
                 expect(convertedMinutes.prefix).toBe('five to');
             }
+        });
+        
+        it('should return a "just before" prefix if it is 2 or less minutes past the hour', function () {
+            var convertedMinutes = minutes.convert(58);
+
+            expect(convertedMinutes.prefix).toBe('just before');
+            convertedMinutes = minutes.convert(59);
+            expect(convertedMinutes.prefix).toBe('just before');
         });
     });
 });
