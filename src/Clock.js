1ab21dda5bcfa611a5e2c988720999179dd6acdd
Ben Smawfield <bensmawfield@googlemail.com>
2012-11-11 16:00:14 +0000

    passing test - returning an hour if there are no minutes and it is am

--- src/Clock.js
+++ src/Clock.js
@@ -1,8 +1,35 @@
 function Clock () {
-
+    this.hours = {
+        '1' : 'one',
+        '2' : 'two',
+        '3' : 'three',
+        '4' : 'four',
+        '5' : 'five',
+        '6' : 'six',
+        '7' : 'seven',
+        '8' : 'eight',
+        '9' : 'nine',
+        '10' : 'ten',
+        '11' : 'eleven'
+        
+    }
 }
 
-Clock.prototype.tellTheTime = function () {
+Clock.prototype.tellHours = function (rawTime) {
+    var hours,
+        rawHours = rawTime.getHours();
+    
+    hours = parseInt(rawHours, 10).toString();
+    
+    return this.hours[hours] + ' o\'clock';
+}
 
-    return '';
+Clock.prototype.tellTheTime = function (rawTime) {
+    var hours = '';
+    
+    if (rawTime && typeof(rawTime.getHours) === 'function') {
+        hours = this.tellHours(rawTime);
+    }
+
+    return hours;
 };
