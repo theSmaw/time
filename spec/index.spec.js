65faca29e61ccfea40b5f201d2e058aa55afd857
Ben Smawfield <bensmawfield@notonthehighstreet.com>
2012-11-12 11:07:31 +0000

    failing spec - minutes field

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -16,6 +16,10 @@ casper.then(function () {
     this.test.assert(this.exists('#hour'), 'should have an hour field');
 });
 
+casper.then(function () {
+    this.test.assert(this.exists('#minutes'), 'should have a minutes field');
+});
+
 casper.run(function () {
     this.test.renderResults(true, 0, 'results/SPEC-index.xml');
 });
