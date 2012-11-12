00832c51236169fb595aeda7703a6abd2b815dc2
Ben Smawfield <bensmawfield@notonthehighstreet.com>
2012-11-12 11:02:25 +0000

    failing spec - hour field

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,10 +1,19 @@
 var casper = require('casper').create();
+var utils = require('utils');
 
 casper.start();
 casper.open('src/index.html');
 
 casper.then(function () {
     this.test.assertTitle('Talking clock', 'should be on the correct page');
+});
+
+casper.then(function () {
+    this.test.assertTitle('Talking clock', 'should be on the correct page');
+});
+
+casper.then(function () {
+    this.test.assert(this.exists('#hour'), 'should have an hour field');
 });
 
 casper.run(function () {
